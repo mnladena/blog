268914cd481648e736d7f15a0bf68829fa80e0c1
adena <adena@detik.com>
2024-04-30 00:05:27 +0700

    update config dan wording

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
   integrations: [mdx(), sitemap()],
   output: "server",
   adapter: vercel(),
