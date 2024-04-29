0f26f37107f85af3d6fa0202e9df09111c7e7b82
adena <adena@detik.com>
2024-04-30 00:08:38 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://myblog-kappa-roan.vercel.app/",
   integrations: [mdx(), sitemap()],
   output: "server",
   adapter: vercel(),
