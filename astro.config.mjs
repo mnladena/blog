f40172e049a8709e6a98493a2e38e8ffcf94b902
adena <adena@detik.com>
2024-04-30 18:46:26 +0700

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
-
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://myblog-kappa-roan.vercel.app/",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
